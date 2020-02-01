e9fdab24f951511bac5a169ad027dee0b8b7a5bb
TilmanChatterbug <58226957+TilmanChatterbug@users.noreply.github.com>
2020-02-01 14:18:15 +0100

    what??

---
+++ AsteroidCrash/Assets/QuestTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToActivate;
+
+    [SerializeField]
+    private GameObject objectToDeactivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
