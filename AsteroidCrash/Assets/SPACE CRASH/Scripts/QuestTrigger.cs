9b8a10861842e222c61a389edfa6fb11ee01278a
TilmanChatterbug <58226957+TilmanChatterbug@users.noreply.github.com>
2020-02-01 14:50:55 +0100

    quest

---
+++ AsteroidCrash/Assets/SPACE CRASH/Scripts/QuestTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToActivate;
+
+    [SerializeField]
+    private GameObject objectToDeactivate;
+
+    [SerializeField]
+    private AudioSource effect;
+
+    [SerializeField]
+    private string QuestItemToReactTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Interactable" 
+            && other.GetComponent<QuestItem>() != null
+            && QuestItemToReactTo == other.GetComponent<QuestItem>().QuestItemName)
+        {
+            if(effect != null)
+            {
+                effect.Play();
+            }
+            objectToActivate.SetActive(true);
+            objectToDeactivate.SetActive(false);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
